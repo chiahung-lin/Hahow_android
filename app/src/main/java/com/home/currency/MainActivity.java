6d2018b1810c3b1951ca612dd826bbcf9a9b8993
Pooh-Lin <chia.hung.lin.bio@gmail.com>
2018-11-30 23:57:39 +0800

    作業二 第三版

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -4,34 +4,40 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edNTD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void trans(View view){
-        EditText edNTD = findViewById(R.id.ntd);
-        String moneyData = edNTD.getText().toString();
-        String title;
-        String message;
-        if (moneyData.equals("")){
-            title = "Problem";
-            message ="Please enter you NTD amount";
-        }
-        else{
-            float money = Float.parseFloat(moneyData) / 30.9f;
-            title="Result";
-            message="USD is "+money;
-        }
-        new AlertDialog.Builder(this)
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton("OK",null)
-                .show();
+        edNTD = findViewById(R.id.ntd);
+        Button btCalcu = findViewById(R.id.btCalcu);
+        btCalcu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String moneyData = edNTD.getText().toString();
+                String title;
+                String message;
+                if (moneyData.equals("")){
+                    title = "Problem";
+                    message ="Please enter you NTD amount";
+                }
+                else{
+                    float money = Float.parseFloat(moneyData) / 30.9f;
+                    title="Result";
+                    message="USD is "+money;
+                }
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle(title)
+                        .setMessage(message)
+                        .setPositiveButton("OK",null)
+                        .show();
+            }
+        });
     }
 }
