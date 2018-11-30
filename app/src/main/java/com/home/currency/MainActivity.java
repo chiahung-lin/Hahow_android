448d15ced79ed90d9f94844ee92b2c25830821cc
Pooh-Lin <chia.hung.lin.bio@gmail.com>
2018-11-30 23:50:35 +0800

    作業二 第一版

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -1,7 +1,10 @@
 package com.home.currency;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void trans(View view){
+        EditText edNTD = findViewById(R.id.ntd);
+        String moneyData = edNTD.getText().toString();
+        if (moneyData.equals("")){
+            new AlertDialog.Builder(this)
+                    .setTitle("Problem")
+                    .setMessage("Please enter you NTD amount")
+                    .setPositiveButton("OK",null)
+                    .show();
+        }
+        else{
+            float money = Float.parseFloat(moneyData) / 30.9f;
+            new AlertDialog.Builder(this)
+                    .setTitle("Result")
+                    .setMessage("USD is "+money)
+                    .setPositiveButton("OK",null)
+                    .show();
+        }
+
     }
 }
